92e1a2be5e164902c8cdc0c6091a85d31ce097c0
Mohammad Sagor Ali <68495729+sagoralways@users.noreply.github.com>
2023-03-25 15:01:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Mohammad Sagor',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -25,7 +25,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Faysal Mahmud',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -44,7 +44,7 @@ const account2 = {
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Easin Easin',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
@@ -63,7 +63,7 @@ const account3 = {
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Zahid Zahid',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
